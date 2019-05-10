b43b0a2d629647a42e8f2e5af3ea2825beba4174
Yongjae Yi <dididy@knou.ac.kr>
2019-05-11 02:49:31 +0900

    Add: assignment's purpose

--- receiver.py
+++ receiver.py
@@ -1,30 +1,36 @@
 import socket
 import sys
+import time
 import select
 
 FLAGS = None
 class ClientSocket():
-    
+
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind((FLAGS.ip, FLAGS.port))
-        self.addr = (FLAGS.ip, FLAGS.port)
+        self.socket.bind(('', FLAGS.port))
         self.buf = 1024
+        self.timeout = 3
 
     def socket_send(self):
-        data,addr = self.socket.recvfrom(self.buf)
-        f = open(data.strip(), 'wb')
+        while True:
+            data, addr = self.socket.recvfrom(self.buf)
+            data_size, addr = self.socket.recvfrom(self.buf)
+            if data:
+                print("file_name:", data.decode())
+                print("file_size:", data_size.decode())
 
-        data,addr = s.recvfrom(self.buf)
-        try:
-            while(data):
-                f.write(data)
-                self.socket.settimeout(2)
-                data,addr = s.recvfrom(self.buf)
-        except timeout:
-            f.close()
-            self.socket.close()
-            print ("File Downloaded")
+            f = open(data.strip(), 'wb')
+
+            while True:
+                ready = select.select([self.socket], [], [], self.timeout)
+                if ready[0]:
+                    data,addr = self.socket.recvfrom(self.buf)
+                    f.write(data)
+            else:
+                print("%s Finish!" % data.strip())
+                f.close()
+                break;
 
     def main(self):
         self.socket_send()
@@ -33,9 +39,8 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--ip', type=str, default='localhost')
-    parser.add_argument('-p', '--port', type=int, default=8080)
-    parser.add_argument('-f', '--filename', type=str)
+    #parser.add_argument('-i', '--ip', type=str, default='127.0.0.1')
+    parser.add_argument('-p', '--port', type=int, default=1234)
 
 
     FLAGS, _ = parser.parse_known_args()
--- sender.py
+++ sender.py
@@ -1,29 +1,55 @@
 import socket
+import time
 import sys
-import select
+import os.path 
+import os
 
 FLAGS = None
 class ClientSocket():
     
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind((FLAGS.ip, FLAGS.port))
         self.buf = 1024
         self.addr = (FLAGS.ip, FLAGS.port)
 
     def socket_send(self):
-        file_name = sys.argv[1]
+        file_name = FLAGS.filename
+        self.socket.sendto(file_name.encode(), self.addr)
+        self.socket.sendto(str(os.path.getsize(file_name)).encode(), self.addr)
+        
+        file_size = int(os.path.getsize(file_name))
+	
+        print("%s is sending now" % file_name)
 
-        self.socket.sendto(file_name.encode('utf-8'),self.addr)
+        f = open(file_name,"rb")
 
-        f=open(file_name,"rb")
+        i = 0
+
         data = f.read(self.buf)
         while (data):
-            if(self.socket.sendto(data,self.addr)):
-                print ("sending...")
+            if(self.socket.sendto(data, self.addr)):
+                print ("dst:", FLAGS.ip, " file_name:", file_name)
+	
                 data = f.read(self.buf)
+                time.sleep(0.02)
+                
+                if(self.buf < file_size):
+                    get_percent = (self.buf * i / file_size * 100)
+                    if(self.value_is_int(get_percent) == True): 
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        print('\r[{0}] {1}%'.format('#'*(int(get_percent/2)), round(get_percent, 4)))
+                        print("current_size / total_size =", self.buf * i, "/", file_size) 
+                    i = i + 1
+
+        print(file_name, "send completely!")
         self.socket.close()
         f.close()
+    
+    def value_is_int(self, value):
+        if (value -  int(value) < 1):
+            return True
+        else:
+            return False
 
     def main(self):
         self.socket_send()
@@ -32,9 +58,9 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--ip', type=str, default='localhost')
-    parser.add_argument('-p', '--port', type=int, default=8080)
-    #parser.add_argument('-f', '--filename', type=str, default='test.txt')
+    parser.add_argument('-i', '--ip', type=str, default='127.0.0.1')
+    parser.add_argument('-p', '--port', type=int, default=1234)
+    parser.add_argument('-f', '--filename', type=str, default='test.txt')
 
 
     FLAGS, _ = parser.parse_known_args()
